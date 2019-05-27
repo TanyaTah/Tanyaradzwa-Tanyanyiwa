b2741c67d36ac4dccac7987c2bec6084ef676ec2
TanyaTah <48945086+TanyaTah@users.noreply.github.com>
2019-05-27 11:22:43 +0200

    Update kNearestNeighbor_Iris_Data.py

--- kNearestNeighbor_Iris_Data.py
+++ kNearestNeighbor_Iris_Data.py
@@ -2,7 +2,6 @@
 # coding: utf-8
 
 #Load data
-
 from sklearn import datasets
 iris = datasets.load_iris()
 #or
@@ -19,8 +18,6 @@ iris.keys()
 
 #Target names
 iris['target_names']
-#
-#iris.target_names
 
 #Feature names
 iris['feature_names']
@@ -31,32 +28,16 @@ type(iris['data'])
 #Shape
 iris['data'].shape
 
-#Finding neighbors
-
+#Finding number of neighbors
 import numpy as np
 neighbors = np.arange(1,9) 
-
 #Accuracy
 train_accuracy = np.empty(len(neighbors))
 test_accuracy = np.empty(len(neighbors))
 
-#Create the model object using the specific algorithm, that is, KNeighbors
-
-for i,k in enumerate(neighbors): knn_model = KNeighborsClassifier(n_neighbors=k)
-print(knn_model)
-
-#Split the data into training and testing data
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size = 0.3, random_state=0)
-
-
-#Fit the Model on the training data
-knn_model.fit(X_train, y_train)
-
+#Visualizing Accuracy and number of neighbors
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
-
 plt.title('k-NN Varying number of neighbors')
 plt.plot(neighbors, test_accuracy, label='Testing Accuracy')
 plt.plot(neighbors, train_accuracy, label='Training accuracy')
@@ -65,16 +46,26 @@ plt.xlabel('Number of neighbors')
 plt.ylabel('Accuracy')
 plt.show()
 
+#Create the model object using the specific algorithm, that is, KNeighbors
+for i,k in enumerate(neighbors): knn_model = KNeighborsClassifier(n_neighbors=k)
+print(knn_model)
+
+#Split the data into training and testing data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size = 0.3, random_state=0)
+
+#Fit the Model on the training data
+knn_model.fit(X_train, y_train)
+
 #Use the model to predict the labels of the test data
 y_predicted = knn_model.predict(X_test)
 
 #View predicted data
 y_predicted
 
-# Check the results using metrics
+# Check the results using Classification Report
 from sklearn import metrics
 y_predicted =knn_model.predict(X_test)
-
 print(metrics.classification_report(y_test, y_predicted))
 #The averaged f1-score is often used as a convenient measure of the overall performance of an algorithm,that is , macro avg
 
